4589de0032f2f50781ad7bc02813f230449f2aa8
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-12-12 20:58:28 -0300

    atividade condori

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const footer = document.querySelector('#footer');
 header.innerHTML = `<nav class="header__container__navs">
         <ul>
             <li><a href="./app.html">Inicio</a></li>
-            <li><a href="./calculo.html">Estátistica</a></li>
+            <li><a href="./calculo.html">Programação Linear</a></li>
             <li><a href="./videos.html">Videos</a></li>
             <li><a href="./ferramentas.html">Ferramentas didáticas</a></li>
             <li><a href="./sobre.html">Sobre</a></li>
         </ul>
         </nav>`;
 
-footer.innerHTML = `<span>©2024 por Levi Pereira da Silva e Arthur Guerra de Souza.</span>
+footer.innerHTML = `<span>©2025 Arthur Guerra de Souza.</span>
         <ul class="footer__container__logos">
             <li class="footer__container__logos__logo">
               <a href="http://www.fateccarapicuiba.edu.br/" target="_blank"
